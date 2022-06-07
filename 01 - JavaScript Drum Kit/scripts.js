7b3965e09a28d0e7d8ebd32b84d13757d679c518
Olly Seber <olly_s@hotmail.co.uk>
2022-06-06 22:00:28 -0700

    Add animation

--- 01 - JavaScript Drum Kit/scripts.js
+++ 01 - JavaScript Drum Kit/scripts.js
@@ -1,8 +1,22 @@
 document.addEventListener("keydown", (e) => {
     const audio = document.querySelector(`audio[data-key=${e.code}]`);
-    const key = document.querySelector(`.key[data-key=${e.code}`);
-    if (audio) {
-        audio.currentTime = 0;
-        audio.play();
+    if (!audio) {
+        return;
     }
+    audio.currentTime = 0;
+    audio.play();
+
+    const key = document.querySelector(`.key[data-key=${e.code}`);
+    key.classList.add("playing");
 });
+
+const keys = document.querySelectorAll(".key");
+
+function removeTransition(e) {
+    if (e.propertyName !== "transform") {
+        return;
+    }
+    e.target.classList.remove("playing");
+}
+
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
